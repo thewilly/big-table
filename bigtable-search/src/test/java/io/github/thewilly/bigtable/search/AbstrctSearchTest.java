fe3250619dd1c1483ea6473cca4e06b4a7c017dc
willy <colunga91@gmail.com>
2019-11-14 21:49:27 +0100

    [bigtable-search] fixes tests

--- bigtable-search/src/test/java/io/github/thewilly/bigtable/search/AbstrctSearchTest.java
+++ bigtable-search/src/test/java/io/github/thewilly/bigtable/search/AbstrctSearchTest.java
@@ -3,6 +3,9 @@ package io.github.thewilly.bigtable.search;
 import io.github.thewilly.bigtable.Bigtable;
 import io.github.thewilly.bigtable.index.BigtableIndex;
 import io.github.thewilly.bigtable.index.algorithm.BigtableIndexAlgorithm;
+import io.github.thewilly.bigtable.index.mutation.AddElementIndexMutation;
+import org.junit.Before;
+import org.junit.Test;
 
 public class AbstrctSearchTest {
 
@@ -11,17 +14,27 @@ public class AbstrctSearchTest {
             (Bigtable<String>) Bigtable
                     .of("test-table")
                     .withMaxSize(10)
-                    .withIndex(new BigtableIndex<String>(
+                    .withIndex(new BigtableIndex(
                             "string-first-index",
                             new BigtableIndexAlgorithm<String>(10) {
                                 @Override
                                 public int[] computeKeys(String element) {
+                                    int[] keys = new int[1];
+                                    keys[0] = element.hashCode() % 10;
                                     return new int[0];
                                 }
                             }))
                     .create();
 
-    public AbstrctSearchTest() {
-        table.
+    @Before
+    public void setUp() {
+        table.getIndex("string-first-index").mutateIndex(new AddElementIndexMutation("hola"));
+        table.getIndex("string-first-index").mutateIndex(new AddElementIndexMutation("que"));
+        table.getIndex("string-first-index").mutateIndex(new AddElementIndexMutation("tal"));
+    }
+
+    @Test
+    public void testUniqueSearch() {
+        System.out.println(table.getIndex("string-first-index").getHitsForElement("hola").length);
     }
 }

d05822e03346cd48f51f680886077c6f3f7d9576
thewilly <colunga91@gmail.com>
2019-11-19 13:42:10 +0100

    [bigtable-search] test fixed

--- bigtable-search/src/test/java/io/github/thewilly/bigtable/search/AbstrctSearchTest.java
+++ bigtable-search/src/test/java/io/github/thewilly/bigtable/search/AbstrctSearchTest.java
@@ -1,40 +1,60 @@
 package io.github.thewilly.bigtable.search;
 
-import io.github.thewilly.bigtable.Bigtable;
-import io.github.thewilly.bigtable.index.BigtableIndex;
-import io.github.thewilly.bigtable.index.algorithm.BigtableIndexAlgorithm;
-import io.github.thewilly.bigtable.index.mutation.AddElementIndexMutation;
+import io.github.thewilly.bigtable.core.BigtableTable;
+import io.github.thewilly.bigtable.core.models.*;
+import io.github.thewilly.bigtable.core.mutation.RowMutation;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.nio.ByteBuffer;
+
 public class AbstrctSearchTest {
 
-  protected final Bigtable<String> table =
-      (Bigtable<String>)
-          Bigtable.of("test-table")
-              .withMaxSize(10)
-              .withIndex(
-                  new BigtableIndex(
-                      "string-first-index",
-                      new BigtableIndexAlgorithm<String>(10) {
-                        @Override
-                        public int[] computeKeys(String element) {
-                          int[] keys = new int[1];
-                          keys[0] = element.hashCode() % 10;
-                          return new int[0];
-                        }
-                      }))
-              .create();
+  protected final BigtableTable table = new BigtableTable("test-table");
+
 
   @Before
   public void setUp() {
-    table.getIndex("string-first-index").mutateIndex(new AddElementIndexMutation("hola"));
-    table.getIndex("string-first-index").mutateIndex(new AddElementIndexMutation("que"));
-    table.getIndex("string-first-index").mutateIndex(new AddElementIndexMutation("tal"));
+      RowCell rc = new RowCell("c_1");
+
+      for(int i = 1; i < 1000; i++) {
+          TableRow tr = new TableRow(1);
+          RowMutation rm = RowMutation.create(tr);
+          VersionableData d = new VersionableData() {
+
+              ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+              long timeStamp = System.currentTimeMillis();
+              byte[] data = buffer.putLong(timeStamp).array();
+              boolean valid = true;
+
+              @Override
+              public boolean isValid() {
+                  return valid;
+              }
+
+              @Override
+              public void invalidate() {
+                  valid = false;
+              }
+
+              @Override
+              public long getTimestampt() {
+                  return timeStamp;
+              }
+
+              @Override
+              public byte[] get() {
+                  return data;
+              }
+          };
+          rm.updateCell("c_1", d);
+
+          table.getRows().add(tr);
+      }
   }
 
   @Test
   public void testUniqueSearch() {
-    System.out.println(table.getIndex("string-first-index").getHitsForElement("hola").length);
+    System.out.println(table.getNumberOfColumns());
   }
 }
